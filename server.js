7aaca30c0273de6ab51f26a424ab024bd22c2d77
Aydin Gungordu <aydinbg@gmail.com>
2025-03-15 16:16:38 +0000

    Add swagger

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Load Swagger documentation
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
+
 app.use(cors());
 app.use(express.json());
+
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const MEMPOOL_API = 'https://mempool.space/api';
 
