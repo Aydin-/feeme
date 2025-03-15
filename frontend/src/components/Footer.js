7ae3db00675a5f0b1440014d55b04dae1c794118
Aydin Gungordu <aydinbg@gmail.com>
2025-03-15 16:19:52 +0000

    Fix footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,27 +6,29 @@ export const Footer = () => {
   const { t } = useLanguage();
 
   return (
-    <div className="footer">
-      <p>{t('designedBy')} • {t('dataProvided')} mempool.space API • {t('stayingSovereign')}</p>
-      <div className="social-links">
-        <a 
-          href="https://github.com/Aydin-" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="social-link"
-        >
-          <GithubIcon />
-          <span>GitHub</span>
-        </a>
-        <a 
-          href="https://www.linkedin.com/in/aydingungordu/" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="social-link"
-        >
-          <LinkedInIcon />
-          <span>LinkedIn</span>
-        </a>
+    <div className="footer glassy-footer">
+      <div className="footer-content">
+        <p>{t('designedBy')} • {t('dataProvided')} mempool.space API • {t('stayingSovereign')}</p>
+        <div className="social-links">
+          <a 
+            href="https://github.com/Aydin-/feeme" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="social-link"
+          >
+            <GithubIcon />
+            <span>GitHub</span>
+          </a>
+          <a 
+            href="https://www.linkedin.com/in/aydingungordu/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="social-link"
+          >
+            <LinkedInIcon />
+            <span>LinkedIn</span>
+          </a>
+        </div>
       </div>
     </div>
   );
